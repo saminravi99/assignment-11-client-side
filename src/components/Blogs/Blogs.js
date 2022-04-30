51b25cb588f101a932a7071d9f2a50e15bdcf8bc
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-30 21:41:47 +0600

    Blog Page Implemented

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Blogs.css"
 
 const Blogs = () => {
+
+
+  const [blogs, setBlogs] = useState([]);
+
+  console.log(blogs);
+
+  useEffect(() => {
+    fetch("https://warehouse-management-saminravi.herokuapp.com/blogs")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }
+    , [])
+
+    const blog = blogs.map((blog) => {
+      return (
+        <div className="container blog-container">
+          <div className="col-12 mb-5">
+            <div className=" p-0 px-lg-5 py-lg-4 py-3">
+              <div className="d-flex align-items-center">
+                <div>
+                  <img className="review-image" src={blog.image} alt="" />
+                </div>
+                <div className="ms-3">
+                  <h5 className="mb-2">{blog.name}</h5>
+                  <h6 className="mb-0">{blog.date}</h6>
+                </div>
+              </div>
+
+              <h3 className="mt-3 text-center">{blog.blogHeading}</h3>
+              <div className="d-flex justify-content-center align-items-center my-4 my-lg-5">
+                <img className="blog-image" src={blog.blogImage} alt="" />
+              </div>
+              <div className="mx-auto blog-detail">
+                <p>{blog.blog}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div className="padding-nav">
-        <h1>Blogs</h1>
+        <div className="row mx-auto my-lg-0 my-4">{blog}</div>
       </div>
     );
 };
